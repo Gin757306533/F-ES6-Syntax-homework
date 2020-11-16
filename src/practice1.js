83221daba6372ad6e60b36af9af088949cf18f21
JieXu <jie.xu@thoughtworks.com>
2020-11-16 23:45:52 +0800

    SP[xj-1]: commit

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
+const parseData = (inputData) => {
+    const {data, column} = inputData;
+    const keys = column.map(item => item.name);
+    return data.map((row) =>
+            row.reduce((res, field, index) => {
+                res[keys[index]] = field;
+                return res;
+            }, {})
+        );
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
+const inject = (items, sections) =>{
+    const result = [...items];
+    sections.sort((section1, section2) => section2.index - section1.index);
+    sections.map((section) =>{
+        result.splice(section.index, 0, section.content);
+    });
+    return result;
+}
 export { inject };
